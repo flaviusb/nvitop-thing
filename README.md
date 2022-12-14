06885a5ce7383a519c91c5a26cdf77b3b7579892
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2022-12-15 10:53:21 +1300

    Add in process information and readme description of the output format.

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ cd nvitop-thing
 . foo/bin/activate
 python3 logging-script-thing.py
 ```
+
+## Output format
+
+The script outputs two sets of csv data interleaved into log files. The log files are rotated weekly. The default base filename is `usage.csv`.
+
+The two formats are
+
+```csv
+"PhysicalGPU",unix time,device.physical_index,device.memory_used,device.memory_free,device.memory_total,\"device.performance_state\",device.power_usage,device.gpu_utilization
+```
+
+and
+
+```csv
+"Process",unix time,process.username,process.name,process.elapsed_time_in_seconds,process.gpu_time,process.gpu_sm_utilization,process.gpu_memory_utilization()
+```
+
+And so you can filter based on whether the first cell is `PhysicalGPU` or `Process`.
--- logging-script-thing.py
+++ logging-script-thing.py
@@ -6,12 +6,12 @@ import logging
 
 lh = logging.getLogger()
 
-def getGraphicsCardUsage(snapshot):
+def getGraphicsCardUsage(snapshot, t):
     out = []
     for device in snapshot.devices:
-      out.append(f"PhysicalGPU,{device.physical_index},{device.memory_used},{device.memory_free},{device.memory_total},\"{device.performance_state}\",{device.power_usage},{device.gpu_utilization}")
+      out.append(f"PhysicalGPU,{t},{device.physical_index},{device.memory_used},{device.memory_free},{device.memory_total},\"{device.performance_state}\",{device.power_usage},{device.gpu_utilization}")
     for process in snapshot.gpu_processes:
-      out.append(f"Process,{process.username()},{process.name()},{process.elapsed_time_in_seconds()},{process.gpu_time()},{process.gpu_sm_utilization()},{process.gpu_memory_utilization()}")
+      out.append(f"Process,{t},{process.username},{process.name},{process.elapsed_time_in_seconds},{process.gpu_time},{process.gpu_sm_utilization},{process.gpu_memory_utilization}")
     return lh.makeRecord("", 10, "", 10, '\n'.join(out), [], "")
 
 file_handler = TimedRotatingFileHandler("usage.csv", backupCount=10000, when='W0')
@@ -19,7 +19,8 @@ x = ResourceMetricCollector()
 
 while True:
     g = x.take_snapshots()
-    data = getGraphicsCardUsage(g)
+    t = time.time()
+    data = getGraphicsCardUsage(g, t)
     file_handler.emit(data)
     # ... output
     time.sleep(300)
