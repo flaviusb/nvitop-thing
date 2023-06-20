3b921492f44d61de1d9800ff62dbfdb8b2e5800b
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2023-06-20 16:46:59 +1200

    Attempt to iterate GPU processes manually, as the snapshot does not seem to capture them.

--- logging-script-thing.py
+++ logging-script-thing.py
@@ -12,12 +12,26 @@ out_location = "usage.csv"
 if len(sys.argv) > 1:
   out_location = sys.argv[1]
 
+import contextlib
+import itertools
+import math
+import os
+import threading
+import time
+from collections import OrderedDict, defaultdict
+from typing import Callable, Generator, Iterable, NamedTuple, TypeVar
+from weakref import WeakSet
+
+from nvitop.api import host
+from nvitop.api.device import CudaDevice, Device
+from nvitop.api.process import GpuProcess, HostProcess
+
 def getGraphicsCardUsage(snapshot, t):
     out = []
     for device in snapshot.devices:
       out.append(f"PhysicalGPU,{t},{device.physical_index},{device.memory_used},{device.memory_free},{device.memory_total},\"{device.performance_state}\",{device.power_usage},{device.gpu_utilization}")
-    for process in snapshot.gpu_processes:
-      out.append(f"Process,{t},{process.username},{process.name},{process.elapsed_time_in_seconds},{process.gpu_time},{process.gpu_sm_utilization},{process.gpu_memory_utilization}")
+      for process in device.real.processes().values():
+          out.append(f"Process,{t},{process.username()},{process.name()},{process.elapsed_time_in_seconds()},{process.gpu_time()},{process.gpu_sm_utilization()},{process.gpu_memory_utilization()}")
     return lh.makeRecord("", 10, "", 10, '\n'.join(out), [], "")
 
 file_handler = TimedRotatingFileHandler(out_location, backupCount=10000, when='W0')
